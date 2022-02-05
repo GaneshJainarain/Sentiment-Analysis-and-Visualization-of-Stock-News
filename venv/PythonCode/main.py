26785c5baf4c08c0b4663f4027351f4fac92237c
Ganesh Jainarain <RicheyJay2@gmail.com>
2022-02-04 20:40:11 -0500

    got data from finviz

--- venv/PythonCode/main.py
+++ venv/PythonCode/main.py
@@ -5,12 +5,17 @@ from bs4 import BeautifulSoup
 finvis_url = 'https://finviz.com/quote.ashx?t='
 tickers = ['SPY', 'AAPL', 'FB']
 
+news_tables = {}
 for ticker in tickers:
     url = finvis_url + ticker
 
     req = Request(url=url, headers={'user-agent': 'my-app'})
     response = urlopen(req)
-    html = BeautifulSoup(response, 'html')
-    print(html)
 
+    html = BeautifulSoup(response, 'html')
+    news_table = html.find(id='news-table')
+    news_tables[ticker] = news_table
+
+    #print(html)
     break
+print(news_tables)
